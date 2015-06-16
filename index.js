7c86b087ab478c797f3ab46e641718e180517851
Steven Ng <steven.ng@braintapper.com>
2015-06-16 11:52:09 -0400

    Wired in update command

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-var fs, help, logger, noOp, pkg, program, result, subcommand;
+var fs, gulp, help, logger, noOp, pkg, program, result, subcommand;
 
 require('sugar');
 
@@ -10,6 +10,8 @@ program = require('commander');
 logger = require('./logger').Logger;
 
 fs = require('fs-extra');
+
+gulp = require('gulp');
 
 noOp = function() {
   return console.log("Nothing ran, couldn't understand your command");
@@ -29,18 +31,18 @@ help.action(function() {
 
 subcommand.init = program.command('init');
 
-subcommand.init.description('Initialize knodeo for the current working folder');
+subcommand.init.description('Initialize cookbook for the current working folder');
 
 subcommand.init.action(function() {
   var confTemplatePath, mainModulePath, source, target;
   mainModulePath = process.mainModule.filename;
   confTemplatePath = mainModulePath.replace('index.js', 'src/recipes/config.cookbook.template.cson');
-  console.log("init confTemplatePath");
-  console.log(confTemplatePath);
   source = confTemplatePath;
   target = "config.cookbook.cson";
   return fs.readFile(target, function(err, paths) {
     if (err) {
+      console.log(err);
+      console.log("should copy config");
       return fs.copySync(source, target);
     } else {
       return logger.warn("Config file already exists. Please manually delete it and try again.");
@@ -55,7 +57,7 @@ subcommand["new"].description('Create a new thing');
 subcommand["new"].action(function() {
   var confTemplatePath, mainModulePath;
   mainModulePath = process.mainModule.filename;
-  confTemplatePath = mainModulePath.replace('knodeo.js', 'recipes/**/*.jade');
+  confTemplatePath = mainModulePath.replace('index.js', 'src/recipes/**/*.jade');
   logger.info("Move recipes from: " + confTemplatePath);
   return gulp.src(confTemplatePath).pipe(gulp.dest("_workshop/recipes"));
 });
--- src/index.coffee
+++ src/index.coffee
@@ -1,1 +1,70 @@
-console.log "HELLO WORLD"
+require 'sugar'
+
+
+# Get listing of valid cloud services
+
+pkg = require('./package.json')
+program = require('commander')
+logger = require('./logger').Logger
+fs = require('fs-extra')
+gulp = require('gulp')
+
+noOp = ()-> 
+  console.log "Nothing ran, couldn't understand your command"
+
+
+#console.log liquibase
+
+# version
+program
+.version(pkg.version, '-v, --version')
+
+subcommand = { }
+
+
+
+# ## `cookbook help`
+
+help = program.command 'help'
+help.description 'Create a new thing'
+help.action ()->
+  require('./about')
+
+
+
+
+
+subcommand.init = program.command 'init'
+subcommand.init.description 'Initialize cookbook for the current working folder'
+subcommand.init.action ()->
+
+    # copy the templates\
+    mainModulePath = process.mainModule.filename
+    
+    confTemplatePath = mainModulePath.replace('index.js','src/recipes/config.cookbook.template.cson')
+
+    
+    source = confTemplatePath
+    target = "config.cookbook.cson"    
+    fs.readFile target, (err, paths) ->
+      if err
+        console.log err
+        console.log "should copy config"
+        fs.copySync source, target
+      else
+        logger.warn "Config file already exists. Please manually delete it and try again."
+    
+# todo upgrade handling new recipes
+
+
+subcommand.new = program.command 'update'
+subcommand.new.description 'Create a new thing'
+subcommand.new.action ()->
+  mainModulePath = process.mainModule.filename
+  confTemplatePath = mainModulePath.replace('index.js','src/recipes/**/*.jade')    
+  logger.info "Move recipes from: #{confTemplatePath}"
+  gulp.src(confTemplatePath).pipe(gulp.dest("_workshop/recipes"))
+
+
+result = program.parse(process.argv)
+
